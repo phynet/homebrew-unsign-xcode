ccbd014082cbf7c33116bb03fef5546639586c0b
Sofía Swidarowicz Andrade <sofia.swidarowicz@edreamsodigeo.com>
2018-03-18 16:44:41 +0100

    update formula

--- unsign-xcode.rb
+++ unsign-xcode.rb
@@ -4,11 +4,11 @@
 class UnsignXcode < Formula
   desc ""
   homepage ""
-  url "https://github.com/phynet/unsign-xcode/archive/v1.0.3.tar.gz"
-  sha256 "147bb53074509636f30771338f60a927ee529081ddb694a609e06a55d6f94dfd"
+  url "https://github.com/phynet/unsign-xcode/archive/v1.0.4.tar.gz"
+  sha256 "b532b2d61727470ae96650e3560937e08689b8a08013e43e14083a6d8a669506"
   # depends_on "cmake" => :build
 
   def install
-     bin.install "unsign-xcode"
+    bin.install "unsign-xcode"
   end
 end
