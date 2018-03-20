91eace8e741411d06bb3ff73c39ac878a622268a
Sofía Swidarowicz Andrade <sofia.swidarowicz@gmail.com>
2018-03-20 16:39:24 +0100

    added more info to packagage

--- unsign-xcode.rb
+++ unsign-xcode.rb
@@ -2,10 +2,11 @@
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class UnsignXcode < Formula
-  desc ""
-  homepage ""
+  desc "A command line tool to unsign Xcode"
+  homepage "https://github.com/phynet/unsign-xcode"
   url "https://github.com/phynet/unsign-xcode/archive/v1.0.4.tar.gz"
   sha256 "b532b2d61727470ae96650e3560937e08689b8a08013e43e14083a6d8a669506"
+  version "1.0.0"
   # depends_on "cmake" => :build
 
   def install
